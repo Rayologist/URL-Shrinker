bb1b37a5a24972ef84eec6164513f6a3bda449a0
Bo-wei Chen <rayologist1002@gmail.com>
2021-09-12 16:58:12 +0800

    import packages

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import express from "express";
+import { DatabaseSetup } from "./models/database/database.js";
+import UrlTable from "./models/database/urlTable.js";
+import methodOverride from "method-override";
+
+const dbSetup = new DatabaseSetup();
+dbSetup.setup();
+
+const dbTable = new UrlTable();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.listen(PORT, () => {
-    `Server listening on port ${PORT}`;
-  });
-  
+  `Server listening on port ${PORT}`;
+});
